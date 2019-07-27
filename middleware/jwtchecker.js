2cb189200eb67caec48e4ebc8c06d3dd0d077cc8
iNspireLeaership <otarudaudu@gmail.com>
2019-07-27 22:50:53 +0100

    error checkers

--- middleware/jwtchecker.js
+++ middleware/jwtchecker.js
@@ -13,12 +13,14 @@ try{
 //verify the presence of a value and handle possible error
 
 if(!token){ 
+	console.log("error line 1")
    res.status(403).json({msg:"user not permitted to access this route"});
   }
 
 //verify token authenticity
 const authJwt = await jwt.verify(token, config.get('JWTsecret'), (err, sessionData) => { 
    if(err){
+   	console.log("error line 2")
    	res.status(403).json('invalid token')
    }
 
