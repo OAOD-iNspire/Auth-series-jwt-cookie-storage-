95ac0cd340184b01ed5ecd86b434c4e0c8cc3ceb
iNspireLeaership <otarudaudu@gmail.com>
2019-07-27 23:05:12 +0100

    update

--- middleware/jwtchecker.js
+++ middleware/jwtchecker.js
@@ -8,19 +8,19 @@ exports.jwtVerify = async (req, res, next) =>{
 // retrieve token value from request
 //const token = req.header('x-auth-token'); 
 
-const token = req.cookies['jwtHolder'];
+const token = req.cookies.jwtHolder;
 try{
 //verify the presence of a value and handle possible error
 
 if(!token){ 
-	console.log("error line 1")
+	console.log("error line 1");
    res.status(403).json({msg:"user not permitted to access this route"});
   }
 
 //verify token authenticity
 const authJwt = await jwt.verify(token, config.get('JWTsecret'), (err, sessionData) => { 
    if(err){
-   	console.log("error line 2")
+   	console.log("error line 2");
    	res.status(403).json('invalid token')
    }
 
