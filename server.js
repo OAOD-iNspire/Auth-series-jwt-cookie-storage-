aacca88e3b3101630d433cf829e1da202551b538
iNspireLeaership <otarudaudu@gmail.com>
2019-07-27 22:29:25 +0100

    update cors

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const baseRoute = require('./routes/index');
 const authController= require('./controller/authentication');
 const cookieParser = require('cookie-parser');
 
+//cors config
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 //connect database
 dbConnection();
 
