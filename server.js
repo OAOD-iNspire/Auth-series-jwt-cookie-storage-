da067c3935c9acfe6add37c6e4c61ca5629d74b4
iNspireLeaership <otarudaudu@gmail.com>
2019-07-27 22:40:05 +0100

    update

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const cookieParser = require('cookie-parser');
 //cors config
 var corsOptions = {
   origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true
 }
 
 //connect database
