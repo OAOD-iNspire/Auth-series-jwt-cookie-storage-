6a8fa587c4f380e5fff365650d4ba61bcf711d2a
iNspireLeaership <otarudaudu@gmail.com>
2019-07-28 16:33:58 +0100

    u

--- controller/authentication.js
+++ controller/authentication.js
@@ -95,7 +95,7 @@ console.log('iran')
                ///console.log(res.set('Set-Cookie', token ))
                    //res.header('Access-Control-Allow-Credentials', 'true');
                    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');          
-                return res.json(token)
+                return console.log(res.json(token))
               
 
 })
