732c69b8d7af47173f1bfd1a6cea269a0d042dae
iNspireLeaership <otarudaudu@gmail.com>
2019-07-28 16:19:11 +0100

    updates

--- controller/authentication.js
+++ controller/authentication.js
@@ -91,11 +91,11 @@ console.log(config.get('JWTsecret'));
 jwt.sign(uniqueId, config.get('JWTsecret'), (err, token ) => {
 console.log('iran')
                    if(err){ console.log('i ran'); throw err}
-                console.log (res.cookie('jwtHolder', token, {domain:'localhost:3000', path: '/', httpOnly: false}));
+               res.cookie('jwtHolder', token, {domain:'localhost:3000', path: '/', httpOnly: false});
                ///console.log(res.set('Set-Cookie', token ))
                    //res.header('Access-Control-Allow-Credentials', 'true');
                    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');          
-                   return console.log(res.status(200).json(token))
+                return console.log(res.status(200).json(token))
               
 
 })
