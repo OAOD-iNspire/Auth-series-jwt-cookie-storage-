d62df407ef1d9180248d67f936b38645e12522b0
iNspireLeaership <otarudaudu@gmail.com>
2019-07-29 12:33:33 +0100

    u

--- middleware/jwtchecker.js
+++ middleware/jwtchecker.js
@@ -9,6 +9,7 @@ exports.jwtVerify = async (req, res, next) =>{
 // retrieve token value from request
 //const token = req.header('x-auth-token'); 
 
+console.log(req.headers)
 const cookies = new Cookies(req.headers.cookie)
 console.log(cookies.get('jwtHolder'))
 
